4ace20e455098e469e1684bbea74f8b5403f891e
Cll <2642137848@qq.com>
2021-03-09 12:56:07 +0800

    ''

--- matchRouteMenu.ts
+++ matchRouteMenu.ts
@@ -3,11 +3,11 @@ import { getUserData } from "@/utils/cookies";
 import { NavigationGuardNext, Route } from "vue-router/types/router";
 
 const whiteList = ['/Login']; 
-
+// 路由守卫
 export function matchRouteMenu(to: Route, from: Route, next: NavigationGuardNext<Vue>) {
   // 
-  console.log(to, from, next);
-  const userData:IRUserData = JSON.parse(getUserData() as string);
+  // console.log(to, from, next);
+  //const userData:IRUserData = JSON.parse(getUserData() as string);
   // if()
   next()
 }
